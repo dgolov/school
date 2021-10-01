c1acf5728735f9d254592514917cc90f2ca845d2
dgolov <Dima5671356713567>
2021-10-01 19:46:11 +0500

    add dev checkout

---
+++ scool-ui/src/views/Profile/Games.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="games">
+    <navbar></navbar>
+    <div class="step landing__section main-section">
+      <div class="page">
+        <div class="container mt-5">
+          <div class="page__inner">
+            <profile-menu :header="header"></profile-menu>
+            <div class="page__main">
+              <p>Игры в разработке</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import Navbar from "../../components/Navbar";
+import ProfileMenu from "../../components/Profile/ProfileMenu";
+
+export default {
+  name: "Games",
+
+  components: {
+    ProfileMenu, Navbar
+  },
+
+  data() {
+    return {
+      header: 'Игры',
+    }
+  },
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
