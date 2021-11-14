4950a29d17ab3d1c16e8f013eca7bbb2cb28b8b0
dgolov <dmitriy.evgenyevich@gmail.com>
2021-11-14 15:35:19 +0500

    add the environment for celery settings

--- school/settings.py
+++ school/settings.py
@@ -307,8 +307,8 @@ CKEDITOR_CONFIGS = {
 
 # CELERY
 
-REDIS_HOST = '0.0.0.0'
-REDIS_PORT = '6379'
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
 CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
