8c17af042067ff73d14765e5bc4720ef7ac75e7b
sv01 <dmitriy.evgenyevich@gmail.com>
2021-11-22 17:35:29 +0000

    Update Redis port

--- school/settings.py
+++ school/settings.py
@@ -309,7 +309,7 @@ CKEDITOR_CONFIGS = {
 # CELERY
 
 REDIS_HOST = os.environ.get('REDIS_HOST')
-REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_PORT = '6379'
 CELERY_BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 CELERY_ACCEPT_CONTENT = ['application/json']
