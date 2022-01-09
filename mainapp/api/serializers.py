711ea179e2f7e95aca27aee680afdf07962a11d7
dgolov <dmitriy.evgenyevich@gmail.com>
2022-01-09 22:29:37 +0300

    Remove task delay in registration serializer

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -196,11 +196,11 @@ class CreateProfileSerializer(serializers.Serializer):
             phone=profile_data['phone'],
             gender=profile_data['gender'],
         )
-        send_mail_task.delay(
-            theme='Регистрация на Future Academy',
-            message='Благодарим за регистрацию',
-            email_to=new_user.email,
-        )
+        # send_mail_task.delay(
+        #     theme='Регистрация на Future Academy',
+        #     message='Благодарим за регистрацию',
+        #     email_to=new_user.email,
+        # )
         return new_user
 
 
