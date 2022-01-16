657d3b489facd8caaf3528a41f4c572201509a38
dgolov <dmitriy.evgenyevich@gmail.com>
2022-01-16 17:54:16 +0300

    Update course page

--- scool-ui/src/views/EducationSingle.vue
+++ scool-ui/src/views/EducationSingle.vue
@@ -128,11 +128,11 @@
 <!--      </div>-->
 <!--    </section>-->
 
-    <section class="profession-info">
+    <section class="profession-info" v-if="course.profession">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <h2 class="program-events-title">Кто такой {{ course.name }}</h2>
+            <h2 class="program-events-title">Кто такой {{ course.profession.name }}</h2>
             <p class="chess-school-decs e-c-desc">{{ course.who_is }}</p>
           </div>
           <div class="col-12">
@@ -205,35 +205,35 @@
       </div>
     </section>
 
-    <section class="earnings">
+    <section class="earnings" v-if="course.category.age_group === 'adults'">
       <div class="container">
         <div class="row">
           <div class="col-12">
-            <h2 class="program-events-title mt-0 mb-5">Сколько зарабатывает веб-разработчик</h2>
+            <h2 class="program-events-title mt-0 mb-5">Сколько зарабатывает {{ course.profession.name }}</h2>
           </div>
           <div class="earnings-inner">
             <div class="col-md-5 col-lg-4">
               <div class="level">
-                <h5 class="level__name">Junior</h5>
+                <h5 class="level__name">{{ course.profession.junior_name }}</h5>
                 <p class="level__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis tellus
                   feugiat, posuere magna et, scelerisque leo.</p>
-                <span class="level__price">от  80 000 ₽ в месяц</span>
+                <span class="level__price">от  {{ course.profession.salary_junior }} ₽ в месяц</span>
               </div>
             </div>
             <div class="col-md-5 col-lg-4">
               <div class="level level_green">
-                <h5 class="level__name">Middle</h5>
+                <h5 class="level__name">{{ course.profession.middle_name }}</h5>
                 <p class="level__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis tellus
                   feugiat, posuere magna et, scelerisque leo.</p>
-                <span class="level__price">от  150 000 ₽ в месяц</span>
+                <span class="level__price">от  {{ course.profession.salary_middle }} ₽ в месяц</span>
               </div>
             </div>
             <div class="col-md-5 col-lg-4">
               <div class="level level_orange">
-                <h5 class="level__name">Middle</h5>
+                <h5 class="level__name">{{ course.profession.senior_name }}</h5>
                 <p class="level__desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis tellus
                   feugiat, posuere magna et, scelerisque leo. </p>
-                <span class="level__price">от  200 000 ₽ в месяц</span>
+                <span class="level__price">от  {{ course.profession.salary_senior }} ₽ в месяц</span>
               </div>
             </div>
           </div>
@@ -421,7 +421,7 @@
     <!--      </div>-->
     <!--    </div>-->
 
-    <section class="employment-business">
+    <section class="employment-business" v-if="course.category.age_group === 'adults'">
       <div class="container">
         <div class="row">
           <div class="col-12">
@@ -453,7 +453,7 @@
       </div>
     </section>
 
-    <section class="summary">
+    <section class="summary" v-if="course.profession">
       <div class="container">
         <div class="row">
           <div class="col-12">
@@ -472,13 +472,13 @@
                     </div>
                     <div class="post-name-wrapp">
                       <p class="post mb-2">Должность:</p>
-                      <h5 class="post-name mt-1">Веб-разработчик</h5>
+                      <h5 class="post-name mt-1">{{ course.profession.name }}</h5>
                     </div>
                   </div>
                   <span class="border-summary border-summary_sm"></span>
                   <div class="money-wrapp">
                     <p class="salary-from mb-2">Зарплата от:</p>
-                    <span class="money mt-1">90 000 ₽</span>
+                    <span class="money mt-1">{{ course.profession.salary_resume }} ₽</span>
                   </div>
                 </div>
                 <span class="border-summary b-s-sm"></span>
@@ -487,18 +487,9 @@
                 <div class="skilss-wrapp">
                   <p class="skilss-title">Профессиональные навыки:</p>
                   <ul class="skills skills-summary">
-                    <li class="skills__item skills__item_size">Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    <li class="skills__item skills__item_size" v-for="skill in course.profession.skills">
+                      {{ skill.text }}
                     </li>
-                    <li class="skills__item skills__item_size">Praesent vulputate nulla sapien, nec auctor nibh congue
-                      eu.
-                    </li>
-                    <li class="skills__item skills__item_size">Lorem ipsum dolor sit amet</li>
-                    <li class="skills__item skills__item_size">Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                    </li>
-                    <li class="skills__item skills__item_size">Praesent vulputate nulla sapien, nec auctor nibh congue
-                      eu.
-                    </li>
-                    <li class="skills__item skills__item_size">Lorem ipsum dolor sit amet</li>
                   </ul>
                 </div>
                 <span class="border-summary"></span>
