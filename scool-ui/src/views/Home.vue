d94fa4a3eb2080a40fed03201f523496e6bd9e98
dgolov <dmitriy.evgenyevich@gmail.com>
2022-06-21 21:51:34 +0300

    Added link in main page

--- scool-ui/src/views/Home.vue
+++ scool-ui/src/views/Home.vue
@@ -143,7 +143,7 @@
                 </a>
               </div>
               <div class="col-md-12">
-                <a href="#">
+                <a href="https://facademy-hsecamp.ru/">
                   <div class="news-block news-block-world py-5 mb-4 white-text eff-h-two"
                        style="background-color: #FFEEF6;">
                     <h2 class="bold my-0">Образовательные программы на базе Высшей Школы Экономики</h2>
