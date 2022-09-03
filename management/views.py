d5b11ec550cabf3f5a0cc471336014fd422daa81
dgolov <dmitriy.evgenyevich@gmail.com>
2022-09-03 23:47:54 +0300

    Added timetable for student detail pages

--- management/views.py
+++ management/views.py
@@ -978,6 +978,7 @@ class StudentDetailView(DetailView, StudentMixin):
         context['title'] = self.get_object()
         context['user'] = self.request.user
         context['academic_performance_list'] = AcademicPerformance.objects.filter(student=self.get_object())
+        context['time_table_list'] = Timetable.objects.filter(group__student_groups=self.get_object())
         return context
 
     def post(self, request, *args, **kwargs):
