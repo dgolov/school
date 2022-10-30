9d9940ba89838836cfbab42329e39fb9c2124f7d
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-30 22:45:58 +0600

    Added active links

--- scool-ui/src/components/Profile/ProfileMenu.vue
+++ scool-ui/src/components/Profile/ProfileMenu.vue
@@ -3,62 +3,66 @@
     <nav class="menu">
       <ul>
         <li>
-          <a href="#" @click="goTo('MyProfile')" class="active">
+          <a href="#" @click="goTo('MyProfile')" :class="getActive('MyProfile')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu1.svg"></span> Моя страница
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('MyCourses')">
+          <a href="#" @click="goTo('MyCourses')" :class="getActive('MyCourses')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu2.svg"></span> Доступные курсы
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('AcademicPerformance')">
+          <a href="#" @click="goTo('TimeTable')" :class="getActive('TimeTable')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu3.svg"></span> Расписание
           </a>
         </li>
         <li>
-          <a href="#">
+          <a href="#" @click="goTo('AcademicPerformance')"
+             :class="getActive('AcademicPerformance')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu4.svg"></span> Успеваемость
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Groups')">
+          <a href="#" @click="goTo('Groups')" :class="getActive('Groups')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu5.svg"></span> Группы
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Friends', {id: $store.state.authUser.id})">
+          <a href="#" @click="goTo('Friends', {id: $store.state.authUser.id})"
+             :class="getActive('Friends')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu6.svg"></span> Друзья
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Followers', {id: $store.state.authUser.id})">
+          <a href="#" @click="goTo('Followers', {id: $store.state.authUser.id})"
+             :class="getActive('Followers')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu7.svg"></span> Подписчики
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Chats')">
+          <a href="#" @click="goTo('Chats')" :class="getActive('Chats')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu8.svg"></span> Сообщения
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Photo', {id: $store.state.authUser.id})">
+          <a href="#" @click="goTo('Photo', {id: $store.state.authUser.id})"
+             :class="getActive('Photo')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu9.svg"></span> Фотографии
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Search')">
+          <a href="#" @click="goTo('Search')" :class="getActive('Search')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu10.svg"></span> Поиск
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Settings')">
+          <a href="#" @click="goTo('Settings')" :class="getActive('Settings')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu11.svg"></span> Настройки
           </a>
         </li>
         <li>
-          <a href="#" @click="goTo('Games')">
+          <a href="#" @click="goTo('Games')" :class="getActive('Games')">
             <span><img src="../../assets/images/cabinet/img/cabinet-menu12.svg"></span> Игры
           </a>
         </li>
@@ -109,6 +113,12 @@ export default {
       });
       this.$store.commit("removeToken")
       window.location.href = '/';
+    },
+
+    getActive(component) {
+      if (component === this.header) {
+        return 'active'
+      }
     },
   }
 }
