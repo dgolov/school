c04e0391067dec09b57289f14a515df90158bfbb
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-30 16:38:45 +0600

    Updated menu in personal profile

--- scool-ui/src/components/Profile/ProfileMenu.vue
+++ scool-ui/src/components/Profile/ProfileMenu.vue
@@ -1,63 +1,85 @@
 <template>
-  <div class="page__menu">
-    <h3>{{ header }}</h3>
-    <hr>
-    <ul class="vMenu">
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/profile.svg" class="menu-icon">
-        <a href="#" @click="goTo('MyProfile')"> Личный кабинет </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/courses.svg" class="menu-icon">
-        <a href="#" @click="goTo('MyCourses')"> Доступные курсы </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/calendar.svg" class="menu-icon">
-        <a href="#" @click="goTo('TimeTable')"> Расписание </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/positive_dynamics.svg" class="menu-icon">
-        <a href="#" @click="goTo('AcademicPerformance')"> Успеваемость </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/group.svg" class="menu-icon">
-        <a href="#" @click="goTo('Groups')"> Мои группы </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/users_people.svg" class="menu-icon">
-        <a href="#" @click="goTo('Friends', {id: $store.state.authUser.id})"> Мои друзья </a>
-<!--        <div class="notification_numbers">1</div>-->
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/followers.svg" class="menu-icon">
-        <a href="#" @click="goTo('Followers', {id: $store.state.authUser.id})"> Мои подписчики </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/message.svg" class="menu-icon">
-        <a href="#" @click="goTo('Chats')"> Мои сообщения </a>
-<!--        <div class="notification_numbers">2</div>-->
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/photos.svg" class="menu-icon">
-        <a href="#" @click="goTo('Photo', {id: $store.state.authUser.id})"> Мои фотографии </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/search.svg" class="menu-icon">
-        <a href="#" @click="goTo('Search')"> Поиск </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/settings.svg" class="menu-icon">
-        <a href="#" @click="goTo('Settings')"> Настройки </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/games.svg" class="menu-icon">
-        <a href="#" @click="goTo('Games')"> Игры </a>
-      </li>
-      <li class="left-align px-2">
-        <img src="../../assets/images/icons/exit.svg" class="menu-icon">
-        <a href="#" @click="logOut()"> Выход </a>
-      </li>
-    </ul>
+  <div class="col-xl-2 col-lg-3">
+    <nav class="menu">
+      <ul>
+        <li>
+          <a href="#" @click="goTo('MyProfile')" class="active">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu1.svg"></span> Моя страница
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('MyCourses')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu2.svg"></span> Доступные курсы
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('AcademicPerformance')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu3.svg"></span> Расписание
+          </a>
+        </li>
+        <li>
+
+          <a href="#">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu4.svg"></span> Успеваемость
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Groups')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu5.svg"></span> Группы
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Friends', {id: $store.state.authUser.id})">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu6.svg"></span> Друзья
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Followers', {id: $store.state.authUser.id})">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu7.svg"></span> Подписчики
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Chats')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu8.svg"></span> Сообщения
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Photo', {id: $store.state.authUser.id})">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu9.svg"></span> Фотографии
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Search')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu10.svg"></span> Поиск
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Settings')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu11.svg"></span> Настройки
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="goTo('Games')">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu12.svg"></span> Игры
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu13.svg"></span> Магазин
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu14.svg"></span> Награды
+          </a>
+        </li>
+        <li>
+          <a href="#" @click="logOut()">
+            <span><img src="../../assets/images/cabinet/img/cabinet-menu15.svg"></span> Выход
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -87,17 +109,11 @@ export default {
         isAuthenticated: false,
       });
       this.$store.commit("removeToken")
-      //this.goTo('Home')
       window.location.href = '/';
     },
   }
 }
 </script>
 
-
 <style scoped>
-.menu-icon {
-  width: 16px;
-  margin-right: 5px;
-}
 </style>
