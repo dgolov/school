939bd0435ee02d695a729fff7262f36f8c5b2e09
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-30 20:11:33 +0600

    Update Profile

--- scool-ui/src/views/Profile/MyProfile.vue
+++ scool-ui/src/views/Profile/MyProfile.vue
@@ -17,20 +17,6 @@
         </div>
       </div>
     </div>
-
-
-<!--    <div class="step landing__section main-section past-events">-->
-<!--      <div class="page">-->
-<!--        <div class="container mt-1">-->
-<!--          <div class="page__inner">-->
-<!--            <profile-menu :header="header"></profile-menu>-->
-<!--            <profile-info v-if="responseData" :profile="responseData"></profile-info>-->
-<!--            <loader v-else object="#ff9633" color1="#ffffff" color2="#17fd3d" size="5" speed="2" bg="#343a40"-->
-<!--                    objectbg="#999793" opacity="80" disableScrolling="false" name="spinning"></loader>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
   </div>
 </template>
 
