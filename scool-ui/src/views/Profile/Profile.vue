9f0c08368c31886a376710b6d27f94befbdae27b
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-30 23:03:17 +0600

    updated other profile pages

--- scool-ui/src/views/Profile/Profile.vue
+++ scool-ui/src/views/Profile/Profile.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="profile">
-    <navbar></navbar>
-    <div class="step landing__section main-section past-events">
-      <div class="page">
-        <div class="container mt-1">
-          <div class="page__inner">
-            <profile-menu :header="header"></profile-menu>
-            <profile-info v-if="responseData" :profile="responseData" @reLoad="createGetRequest(`/profile/${id}`)"></profile-info>
-            <loader v-else object="#ff9633" color1="#ffffff" color2="#17fd3d" size="5" speed="2" bg="#343a40"
-                    objectbg="#999793" opacity="80" disableScrolling="false" name="spinning"></loader>
+    <div class="cabinet-page">
+      <div class="container">
+        <button class="cabinet-menu-button">МЕНЮ ЛИЧНОГО КАБИНЕТА</button>
+        <div class="row">
+          <profile-menu :header="header"></profile-menu>
+          <profile-info v-if="responseData" :profile="responseData"
+                        @reLoad="createGetRequest(`/profile/${id}`)"></profile-info>
+          <div v-if="responseData" class="col-xl-2 col-lg-3"></div>
+          <loader v-else object="#ff9633" color1="#ffffff" color2="#17fd3d" size="5" speed="2" bg="#343a40"
+                  objectbg="#999793" opacity="80" disableScrolling="false" name="spinning"></loader>
+          <div class="col-xl-10 col-lg-9">
+            <div class="cabinet-copy">
+              © Академия будущего «ХОД», 2022
+            </div>
           </div>
         </div>
       </div>
