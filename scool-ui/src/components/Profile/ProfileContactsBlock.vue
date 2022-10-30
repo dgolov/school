a24bea5959b600e31f3ed3071ebe4e0f37a88317
dgolov <dmitriy.evgenyevich@gmail.com>
2022-10-30 20:30:36 +0600

    Update Profile contacts

--- scool-ui/src/components/Profile/ProfileContactsBlock.vue
+++ scool-ui/src/components/Profile/ProfileContactsBlock.vue
@@ -1,48 +1,25 @@
 <template>
-  <div id="profile-contacts">
-    <div class="row my-3">
-      <div class="col-md-3">
-        <h6>Телефон:</h6>
-      </div>
-      <div class="col-md-9 left-align">
-        <a href="tel:+79082373971">{{ profile.phone }}</a>
-      </div>
-    </div>
-    <div class="row my-3">
-      <div class="col-md-3">
-        <h6>Город:</h6>
-      </div>
-      <div class="col-md-9 left-align">
-        <a v-if="profile.city" href="#">{{ profile.city }}</a>
-        <p v-else class="my-0">-</p>
-      </div>
-    </div>
-    <div class="row my-3">
-      <div class="col-md-3">
-        <h6>E-mail:</h6>
-      </div>
-      <div class="col-md-9 left-align">
-        <a href="mailto:vanya_grishin@mail.ru">{{ profile.email }}</a>
-      </div>
-    </div>
-    <div class="row my-3">
-      <div class="col-md-3">
-        <h6>Профиль vk.com:</h6>
-      </div>
-      <div class="col-md-9 left-align">
-        <a v-if="profile.vk_slug" href="https://vk.com/piupiu7">{{ profile.vk_slug }}</a>
-        <p v-else class="my-0">-</p>
-      </div>
-    </div>
-    <div class="row mt-3 mb-4">
-      <div class="col-md-3">
-        <h6>Профиль Instagram:</h6>
-      </div>
-      <div class="col-md-9 left-align">
-        <a v-if="profile.instagram_slug" href="#">{{ profile.instagram_slug }}</a>
-        <p v-else class="my-0">-</p>
-      </div>
-    </div>
+  <div class="item">
+    <p>Контактная информация:</p>
+    <table>
+      <tr>
+        <td>Телефон:</td>
+        <td><a :href="`tel:${ profile.phone }`">{{ profile.phone }}</a></td>
+      </tr>
+      <tr>
+        <td>Город:</td>
+        <td v-if="profile.city">{{ profile.city }}</td>
+        <td v-else>Не указан</td>
+      </tr>
+      <tr>
+        <td>E-mail:</td>
+        <td><a :href="`mailto:${ profile.email }`">{{ profile.email }}</a></td>
+      </tr>
+      <tr>
+        <td>Профиль VK:</td>
+        <td><a v-if="profile.vk_slug" :href="`${ profile.vk_slug }`">{{ profile.vk_slug }}</a></td>
+      </tr>
+    </table>
   </div>
 </template>
 
