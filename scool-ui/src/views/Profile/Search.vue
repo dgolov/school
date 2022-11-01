9bb231a78abcda86fc66fa64ef3a88afd09d3b72
dgolov <dmitriy.evgenyevich@gmail.com>
2022-11-02 02:50:54 +0600

    Updated search page

--- scool-ui/src/views/Profile/Search.vue
+++ scool-ui/src/views/Profile/Search.vue
@@ -1,33 +1,24 @@
 <template>
-  <div id="search">
-    <navbar></navbar>
-    <div class="step landing__section main-section past-events">
-      <div class="page">
-        <div class="container mt-1">
-          <div class="page__inner">
-            <profile-menu :header="header"></profile-menu>
-            <div class="page__main">
-              <div class="row">
-                <div class="col-md-9">
-                  <form>
-                    <input type='text' placeholder="Поиск" class="w-100">
-                  </form>
-                </div>
-                <div class="col-md-3">
-                  <button class="gray-button filter-button">Фильтр</button>
-                </div>
-              </div>
-              <div class="row my-3">
-                <div class="col-md-6">
-                  <a href="#" @click="getStudents()">Студенты</a>
-                </div>
-                <div class="col-md-6">
-                  <a href="#" @click="getTeachers()">Преподаватели</a>
-                </div>
-              </div>
-              <hr/>
-              <profiles-list :profiles="responseData" @reLoad="reloadList()"></profiles-list>
+  <div class="cabinet-page">
+    <div class="container">
+      <button class="cabinet-menu-button">МЕНЮ ЛИЧНОГО КАБИНЕТА</button>
+      <div class="row">
+        <profile-menu :header="header"></profile-menu>
+        <div class="col-xl-10 col-lg-9">
+          <div class="row mb-5 mt-3">
+            <div class="col-md-6 px-5">
+              <a href="#" class="user-menu" @click="getStudents()">Студенты</a>
             </div>
+            <div class="col-md-6 px-5">
+              <a href="#" class="user-menu" @click="getTeachers()">Преподаватели</a>
+            </div>
+          </div>
+          <profiles-list :profiles="responseData" @reLoad="reloadList()"></profiles-list>
+        </div>
+        <div class="col-xl-2 col-lg-3"></div>
+        <div class="col-xl-10 col-lg-9">
+          <div class="cabinet-copy">
+            © Академия будущего «ХОД», 2022
           </div>
         </div>
       </div>
@@ -54,7 +45,7 @@ export default {
 
   data() {
     return {
-      header: 'Поиск',
+      header: 'Search',
       searchUserGroup: ''
     }
   },
@@ -88,7 +79,7 @@ export default {
 
 
 <style scoped>
-.filter-button {
-  height: 68%;
+.user-menu {
+  text-decoration: none;
 }
 </style>
