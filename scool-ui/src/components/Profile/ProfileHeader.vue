0201dafdf1c96f8c266e26a924d60e7e3a8d1de0
dgolov <dmitriy.evgenyevich@gmail.com>
2022-11-07 02:25:36 +0600

    Refactoring

--- scool-ui/src/components/Profile/ProfileHeader.vue
+++ scool-ui/src/components/Profile/ProfileHeader.vue
@@ -8,28 +8,6 @@
         <small v-else-if="profile.user_group === 'teacher'" class="center">Преподаватель</small>
         <small v-else-if="profile.user_group === 'manager'" class="center">Менеджер учебного процесса</small>
       </div>
-      <!--      <div class="col-md-6">-->
-      <!--        <button v-if="profile.id === $store.state.authUser.id" class="btn" @click="goTo('Settings')">-->
-      <!--          Редактировать профиль-->
-      <!--        </button>-->
-      <!--        <button v-if="isFriend(profile.user)" class="w-100" @click="removeFriend(profile, 'profile')">-->
-      <!--          Удалить из друзей-->
-      <!--        </button>-->
-      <!--        <button v-else-if="isSubscription(profile.user)" class="w-100" @click="unsubscribe(profile, 'profile')">-->
-      <!--          Отписаться-->
-      <!--        </button>-->
-      <!--        <button v-else-if="isFollower(profile.user)" class="w-100" @click="addFriendRequest(profile, 'profile')">-->
-      <!--          Принять дружбу-->
-      <!--        </button>-->
-      <!--        <button v-else-if="profile.id !== $store.state.authUser.id" class="w-100"-->
-      <!--                @click="addFriend(profile, 'profile')">-->
-      <!--          Добавить в друзья-->
-      <!--        </button>-->
-      <!--        <button v-if="isFriend(profile.user)" class="w-100" @click="showMessageModal()">-->
-      <!--          Написать сообщение-->
-      <!--        </button>-->
-      <!--        <message-modal v-if="isFriend(profile.user)" :id="profile.id" ref="messageModal"></message-modal>-->
-      <!--      </div>-->
       <profile-statistic-block :profile="profile"></profile-statistic-block>
     </div>
   </div>
