7de80faf60a75b94d1eb4808e34f44c55267b5d7
dgolov <dmitriy.evgenyevich@gmail.com>
2022-11-06 19:09:07 +0600

    Added loader for timetable

--- scool-ui/src/views/Profile/TimeTable.vue
+++ scool-ui/src/views/Profile/TimeTable.vue
@@ -5,12 +5,14 @@
         <button @click="openProfileMenu" class="cabinet-menu-button">МЕНЮ ЛИЧНОГО КАБИНЕТА</button>
         <div class="row">
           <profile-menu :header="header"></profile-menu>
-          <div class="col-xl-10 col-lg-9">
+          <div v-if="isLoaded" class="col-xl-10 col-lg-9">
             <div class="cabinet-content">
               <FullCalendar :options="calendarOptions" />
             </div>
           </div>
-          <div class="col-xl-2 col-lg-3"></div>
+          <div v-if="isLoaded" class="col-xl-2 col-lg-3"></div>
+          <loader v-else object="#ff9633" color1="#ffffff" color2="#17fd3d" size="5" speed="2" bg="#343a40"
+                  objectbg="#999793" opacity="80" disableScrolling="false" name="spinning"></loader>
           <div class="col-xl-10 col-lg-9">
             <div class="cabinet-copy">
               © Академия будущего «ХОД», 2022
@@ -63,6 +65,7 @@ export default {
       addButton: false,
       cancelButton: false,
       selectedCourse: false,
+      isLoaded: false,
       groupId: 0,
       lessonId: 0,
       date: '',
@@ -119,6 +122,7 @@ export default {
           })
         }
       }
+      this.isLoaded = true;
     },
 
     addToTimeTable() {
