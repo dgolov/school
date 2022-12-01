5c2657d968c6c9025a2ca0fe5240f21f3128f15d
dgolov <dmitriy.evgenyevich@gmail.com>
2022-12-01 09:42:23 +0300

    Updated lesson page

--- scool-ui/src/views/Profile/Lesson.vue
+++ scool-ui/src/views/Profile/Lesson.vue
@@ -1,35 +1,65 @@
 <template>
-  <div id="lesson-single">
-    <div class="container lesson past-events">
-      <course-info v-if="responseData" :course="responseData.course"></course-info>
-      <div class="course__section">
-        <div class="container pb-1">
-          <div class="course-info">
-            <h2 class="my-4 system-color">{{responseData.theme}}</h2>
-            <p class="course-text">{{responseData.description}}</p>
+  <div class="cabinet-page">
+    <div class="container">
+      <button @click="openProfileMenu" class="cabinet-menu-button">МЕНЮ ЛИЧНОГО КАБИНЕТА</button>
+      <div class="row">
+        <profile-menu :header="header"></profile-menu>
+        <div class="col-xl-10 col-lg-9">
+          <div class="cabinet-content">
+            <div class="course">
+              <div class="top-text">
+                <a href="#">Назад</a>
+                {{ responseData.course.name }} - {{ responseData.theme }}
+              </div>
+              <div class="tbl">
+                <table>
+                  <tr>
+                    <td>Категория:</td>
+                    <td v-if="responseData.course.category.name">{{ responseData.course.category.name }}</td>
+                    <td v-else>Не указано</td>
+                  </tr>
+                  <tr>
+                    <td>Номер урока:</td>
+                    <td v-if="responseData.lesson_number">{{ responseData.lesson_number }}</td>
+                    <td v-else>Не указано</td>
+                  </tr>
+                </table>
+              </div>
+              <div class="info">
+                <div class="name">
+                  Описание урока:
+                </div>
+                <p>{{ responseData.description }}</p>
+              </div>
+              <div class="center mt-5 tbl">
+                <div v-if="responseData.video_slug" class="mb-5 mt-1">
+                  <h4 class="video-header">Видео урока:</h4>
+                </div>
+                <div v-if="responseData.video_slug" class="mb-4">
+                  <iframe width="560" height="315" :src="responseData.video_slug"
+                          title="YouTube video player" frameborder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          allowfullscreen>
+                  </iframe>
+                </div>
+              </div>
+              <div v-if="responseData.materials" class="mt-5">
+                <a href="#" @click="responseData.materials" download class="course-text">
+                  <button class="cabinet-link-button">Скачать материал к уроку</button>
+                </a>
+              </div>
+              <div v-if="responseData.material_link">
+                <a :href="responseData.material_link" class="course-text">
+                  <button class="cabinet-link-button">Материалы к уроку</button>
+                </a>
+              </div>
+            </div>
           </div>
-          <div class="center mt-5">
-            <div v-if="responseData.video_slug" class="mb-5 mt-1">
-              <img src="../../assets/images/icons/youtube-video-social-media-play_icon-icons.com_59108.png"
-                   class="video-image">
-              <h3 class="video-header">Онлайн трансляция урока:</h3>
-            </div>
-            <div v-if="responseData.video_slug" class="mb-4">
-              <iframe width="560" height="315" :src="responseData.video_slug"
-                      title="YouTube video player" frameborder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      allowfullscreen></iframe>
-            </div>
-            <hr v-if="responseData.materials && responseData.video_slug" />
-            <div v-if="responseData.materials" class="mt-5 system-color">
-              <img src="../../assets/images/icons/download.png" class="download-button">
-              <a href="#" @click="responseData.materials" download class="course-text">Скачать материал к уроку</a>
-              {{responseData.materials}}
-            </div>
-          </div>
-          <hr v-if="responseData.material_link && (responseData.video_slug || responseData.materials)" />
-          <div v-if="responseData.material_link" class="mt-5 system-color">
-            <a :href="responseData.material_link" class="course-text">Ссылка на диск с материалами</a>
+        </div>
+        <div class="col-xl-2 col-lg-3"></div>
+        <div class="col-xl-10 col-lg-9">
+          <div class="cabinet-copy">
+            © Академия будущего «ХОД», 2022
           </div>
         </div>
       </div>
@@ -43,13 +73,14 @@ import CourseInfo from "../../components/Course/CourseInfo";
 import {requestsMixin} from "../../components/mixins/requestsMixin";
 import {redirect} from "../../components/mixins/redirect";
 import {openMenu} from "../../components/mixins/openMenu";
+import ProfileMenu from "../../components/Profile/ProfileMenu";
 
 export default {
   title: 'Академия будущего | Личный кабинет',
   name: "Lesson",
 
   components: {
-    Navbar, CourseInfo
+    Navbar, CourseInfo, ProfileMenu
   },
 
   props: {
@@ -58,7 +89,7 @@ export default {
   },
 
   mixins: [
-      requestsMixin, redirect, openMenu
+    requestsMixin, redirect, openMenu
   ],
 
   created() {
@@ -68,34 +99,24 @@ export default {
 </script>
 
 <style scoped>
-  .lesson {
-    padding-top: 24pt;
-  }
+.course-text {
+  font-size: 18px;
+}
 
-  .course-poster {
-    width: 300px;
-    border-radius: 5%;
-  }
+.video-header {
+  display: inline-block;
+  margin-top: 30px;
+}
 
-  .course-info {
-    border-bottom: 1px solid gray;
-  }
-
-  .course-text {
-    font-size: 18px;
-  }
-
-  .video-image {
-    margin-right: 5px;
-  }
-
-  .video-header {
-    display: inline-block;
-    margin-top: 30px;
-  }
-
-  .download-button {
-    margin-right: 5px;
-    width: 26px;
-  }
+.cabinet-link-button {
+  width: 100%;
+  height: 50px;
+  -webkit-border-radius: 6px;
+  -moz-border-radius: 6px;
+  border-radius: 6px;
+  color: #fff;
+  font-weight: 700;
+  margin-bottom: 30px;
+  background: linear-gradient(271.4deg, #27aae1 0.31%, #d851ff 99.71%);
+}
 </style>
