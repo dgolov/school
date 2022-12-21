0e9feee74f3b800d44667a82563c8638dc958230
dgolov <dmitriy.evgenyevich@gmail.com>
2022-12-21 20:52:34 +0300

    Fixed email settings

--- school/settings.py
+++ school/settings.py
@@ -218,7 +218,7 @@ CHANNEL_LAYERS = {
 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
